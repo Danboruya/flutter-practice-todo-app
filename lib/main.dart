55179842169829d235d180998392710d80aefa97
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2019-06-23 15:23:34 +0900

    Apply dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,23 @@ class TodoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'My Todo List',
+        theme: ThemeData(
+          // Define the default brightness and colors.
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600],
+
+          // Define the default font family.
+          fontFamily: 'Montserrat',
+
+          // Define the default TextTheme. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          /* textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),*/
+        ),
       home: new TodoList()
     );
   }
